42e975acca74622a83f0f431785e17dca0bba978
Matheus Hudson de Souza <matheushudson2013@gmail.com>
2020-10-26 20:23:24 -0300

    Initial commit

--- app/src/main/java/com/example/snackbar/ui/GastosFragment.kt
+++ app/src/main/java/com/example/snackbar/ui/GastosFragment.kt
@@ -14,7 +14,12 @@ class GastosFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gasto, container, false)
+        val view:View = inflater.inflate(R.layout.fragment_gasto, container, false)
+        return view
+
+
+
+
     }
 
     companion object{
