5f72a986ee4dbcaa404cd1a651cff70a8a400ee6
Matheus Hudson de Souza <matheushudson2013@gmail.com>
2020-10-26 21:28:29 -0300

    Exercicios

---
+++ app/src/main/java/com/example/snackbar/domain/CadastrarGastos.kt
@@ -0,0 +1,6 @@
+package com.example.snackbar.domain
+
+import java.util.*
+
+data class CadastrarGastos(var descricao: String, var categoria:String, var dataHora:Date,val valor: String)
+
--- app/src/main/java/com/example/snackbar/ui/GastosFragment.kt
+++ app/src/main/java/com/example/snackbar/ui/GastosFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
 import com.example.snackbar.R
+import com.example.snackbar.domain.CadastrarGastos
+import kotlinx.android.synthetic.main.fragment_gasto.view.*
+import java.sql.Date
+import java.text.NumberFormat
+import java.util.*
 
 class GastosFragment : Fragment() {
+    private var current: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,11 +23,33 @@ class GastosFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view:View = inflater.inflate(R.layout.fragment_gasto, container, false)
+        view.cadastrarButton.setOnClickListener {
+            var date = Date(view.datahoratext.text.toString())
+            var valor =  view.valorText.text.toString()
+
+            val gastos = CadastrarGastos(view.descricaoText.text.toString(),view.categoriaText.text.toString()
+            , date, valor)
+        }
+
+        view.valorText.doOnTextChanged { s,
+            start,
+            before,
+            count
+            ->
+                if (s.toString() != current) {
+
+                    val cleanString: String = s!!.replace("""[R$,.]""".toRegex(), "")
+                    val parsed = cleanString.toDouble()
+                    val formatted = NumberFormat.getCurrencyInstance().format((parsed / 100))
+
+                    current = formatted
+                    view.valorText.setText(formatted)
+                    view.valorText.setSelection(formatted.length)
+
+                }
+            }
+
         return view
-
-
-
-
     }
 
     companion object{
